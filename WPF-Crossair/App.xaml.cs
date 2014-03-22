b7d8b6ba3b81c85097157800d81bd7fa58c00243
Andrey Byelogurov <byelogurov@gmail.com>
2014-03-21 19:06:06 -0700

    First Commit.

---
+++ WPF-Crossair/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Crossair {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+	}
+}
---
+++ WPF-Crossair/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WPF_Crossair {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+
+			String path = System.IO.Path.Combine(Environment.CurrentDirectory, "ret.png");
+			BitmapImage image = new BitmapImage();
+			image.BeginInit();
+			image.UriSource = new Uri(path);
+			image.EndInit();
+
+			this.WindowStyle = System.Windows.WindowStyle.None;
+			this.AllowsTransparency = true;
+			this.Background = Brushes.Transparent;
+			this.Focusable = false;
+			//this.Foreground = true;
+			this.ShowInTaskbar = false;
+			this.Topmost = true;
+			//this.WindowStyle = System.Windows.WindowStyle.None;
+			//this.Style
+
+
+			//this.SizeToContent = true;
+			this.Width = image.Width;
+			this.Height = image.Height;
+
+			Test.Source = image;
+			Test.Width = image.Width;
+			Test.Height = image.Height;
+
+		}
+	}
+}
