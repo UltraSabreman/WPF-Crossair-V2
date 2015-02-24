47942068b188ffc1af85397a1d9e609c4df46847
Andrey Byelogurov <byelogurov@gmail.com>
2015-02-24 14:38:29 -0800

    Working options
    Still need to fix and add a few things but it's almost done.

--- WPF-Crossair/UI/Options.xaml.cs
+++ WPF-Crossair/UI/Options.xaml.cs
@@ -21,104 +21,17 @@ namespace WPF_Crosshair {
 	/// Interaction logic for Options.xaml
 	/// </summary>
 	public partial class Options : Window {
-	
-		//private GameWindow tempWindow = null;
 
-		//public delegate void Done(GameWindow wind);
-		//public event Done OnAccept;
+		OptionsModel model;
 
 
 		public Options() {
-			//tempWindow = new GameWindow(null, null);
-			//tempWindow.isEnabled = false;
-
 			InitializeComponent();
-			
-			FilePath.Text = Configs.Properties["ImagePath"] as String;
-			HotKey key = Configs.convertTo<HotKey>(Configs.Properties["HotKey"]);
-			if (key == null)
-				ToggleBind.KeyBind = new List<Keys>();
-			else
-				ToggleBind.KeyBind = key.KeyList;
 
-			ToggleBind.updateText();
-			ExitWith.IsChecked = (bool) Configs.Properties["ExitWithProgram"];
-			TargetWindow.Text = Configs.Properties["TargetTitle"] as String;
-
-			FilePath.TextWrapping = TextWrapping.NoWrap;
-			this.ResizeMode = System.Windows.ResizeMode.NoResize;
+			model = new OptionsModel(this);
+			DataContext = model;
 		}
 
-		private void ReloadButton_Click(object sender, RoutedEventArgs e) {
-			try {
-				//tempWindow.LoadImage(Configs.Properties["ImagePath"] as String);
-				System.Windows.MessageBox.Show("Crosshair loaded successfully!", "Loaded", System.Windows.MessageBoxButton.OK, MessageBoxImage.Information);			
-			} catch (FileLoadException) {
-				System.Windows.MessageBox.Show("Crosshair failed to load, is it corrupted?", "Failed to load", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);			
-			} catch (FileNotFoundException) {
-				System.Windows.MessageBox.Show("Crosshair file not found.", "File not found", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);			
-			}
-
-			FilePath.Text = Configs.Properties["ImagePath"] as String;
-			HotKey key = Configs.convertTo<HotKey>(Configs.Properties["HotKey"]);
-			if (key == null)
-				ToggleBind.KeyBind = new List<Keys>();
-			else
-				ToggleBind.KeyBind = key.KeyList;
-
-			ToggleBind.updateText();
-			ExitWith.IsChecked = (bool)Configs.Properties["ExitWithProgram"];
-			TargetWindow.Text = Configs.Properties["TargetTitle"] as String;
-		}
-
-		private void OkButton_Click(object sender, RoutedEventArgs e) {
-			Configs.Properties["ImagePath"] = FilePath.Text;
-
-			try {
-				//tempWindow.LoadImage(Configs.Properties["ImagePath"] as String);			
-			} catch (FileLoadException) {
-				System.Windows.MessageBox.Show("Crosshair failed to load, is it corrupted?", "Failed to load", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);
-				return;
-			} catch (FileNotFoundException) {
-				System.Windows.MessageBox.Show("Crosshair file not found.", "File not found", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);
-				return;
-			}
-
-			Configs.Properties["ImagePath"] = FilePath.Text;
-			Configs.Properties["HotKey"] = new HotKey(ToggleBind.KeyBind);
-			Configs.Properties["ExitWithProgram"] = ExitWith.IsChecked;
-			Configs.Properties["TargetTitle"] = TargetWindow.Text;
-
-			//if (OnAccept != null)
-			//	OnAccept(tempWindow);
-
-			this.Close();
-		}
-
-		private void CancelButton_Click(object sender, RoutedEventArgs e) {
-			this.Close();
-		}
-
-		private void BrowseButton_Click(object sender, RoutedEventArgs e) {
-			Microsoft.Win32.OpenFileDialog diag = new Microsoft.Win32.OpenFileDialog();
-			diag.InitialDirectory = Directory.GetCurrentDirectory();
-			diag.CheckFileExists = true;
-			diag.DefaultExt = "png";
-			diag.Filter = "PNG Image (*.png)|*.png";
-			diag.Multiselect = false;
-			
-
-			// Process open file dialog box results 
-			if (diag.ShowDialog() == true)
-			{ 
-				string filename = diag.FileName;
-				FilePath.Text = filename;
-			}
-		}
-
-		private void TestTarget_Click(object sender, RoutedEventArgs e) {
-			//tempWindow.setWindowTitleRegex(TargetWindow.Text);
-			//tempWindow.test();
-		}
+		
 	}
 }
--- WPF-Crossair/UI/ViewModels/OptionsModel.cs
+++ WPF-Crossair/UI/ViewModels/OptionsModel.cs
@@ -5,6 +5,115 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WPF_Crosshair {
-	class OptionsModel {
+	using System.IO;
+	using System.Windows;
+	using Keys = System.Windows.Forms.Keys;
+	class OptionsModel : ViewModelNotifier {
+		public String FilePath { get { return GetProp<String>(); } set { SetProp(value); } }
+		public String TargetWindow { get { return GetProp<String>(); } set { SetProp(value); } }
+		public bool ExitWithProgram { get { return GetProp<bool>(); } set { SetProp(value); } }
+			
+		//Hack, find a way to bind this
+		private Options options;
+
+		//TODO: Handle file exceptions
+		//TODO: Handle binds + special cases better
+		//TODO: Handle test button
+
+		public OptionsModel(Options b) {
+			options = b;
+
+			FilePath = Configs.Properties["ImagePath"] as String;
+			HotKey key = Configs.convertTo<HotKey>(Configs.Properties["HotKey"]);
+			if (key == null)
+				options.ToggleBind.KeyBind = new List<Keys>();
+			else
+				options.ToggleBind.KeyBind = key.KeyList;
+
+			options.ToggleBind.updateText();
+
+			ExitWithProgram = (bool)Configs.Properties["ExitWithProgram"];
+			TargetWindow = Configs.Properties["TargetTitle"] as String;
+
+			options.CancelButton.Click += CancelButton_Click;
+			options.OkButton.Click += OkButton_Click;
+			options.ReloadButton.Click += ReloadButton_Click;
+			options.BrowseButton.Click += BrowseButton_Click;
+			options.TestTarget.Click += TestTarget_Click;
+		}
+	
+
+	private void ReloadButton_Click(object sender, RoutedEventArgs e) {
+			try {
+				//tempWindow.LoadImage(Configs.Properties["ImagePath"] as String);
+				System.Windows.MessageBox.Show("Crosshair loaded successfully!", "Loaded", System.Windows.MessageBoxButton.OK, MessageBoxImage.Information);			
+			} catch (FileLoadException) {
+				System.Windows.MessageBox.Show("Crosshair failed to load, is it corrupted?", "Failed to load", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);			
+			} catch (FileNotFoundException) {
+				System.Windows.MessageBox.Show("Crosshair file not found.", "File not found", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);			
+			}
+
+			FilePath = Configs.Properties["ImagePath"] as String;
+			HotKey key = Configs.convertTo<HotKey>(Configs.Properties["HotKey"]);
+			if (key == null)
+				options.ToggleBind.KeyBind = new List<Keys>();
+			else
+				options.ToggleBind.KeyBind = key.KeyList;
+
+			options.ToggleBind.updateText();
+			ExitWithProgram = (bool)Configs.Properties["ExitWithProgram"];
+			TargetWindow = Configs.Properties["TargetTitle"] as String;
+		}
+
+		private void OkButton_Click(object sender, RoutedEventArgs e) {
+			Configs.Properties["ImagePath"] = FilePath;
+
+			try {
+				//tempWindow.LoadImage(Configs.Properties["ImagePath"] as String);			
+			} catch (FileLoadException) {
+				System.Windows.MessageBox.Show("Crosshair failed to load, is it corrupted?", "Failed to load", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			} catch (FileNotFoundException) {
+				System.Windows.MessageBox.Show("Crosshair file not found.", "File not found", System.Windows.MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			Configs.Properties["ImagePath"] = FilePath;
+			Configs.Properties["HotKey"] = new HotKey(options.ToggleBind.KeyBind);
+			Configs.Properties["ExitWithProgram"] = ExitWithProgram;
+			Configs.Properties["TargetTitle"] = TargetWindow;
+			Configs.Properties.Save();
+
+			//if (OnAccept != null)
+			//	OnAccept(tempWindow);
+
+			options.Close();
+		}
+
+		private void CancelButton_Click(object sender, RoutedEventArgs e) {
+			options.Close();
+		}
+
+		private void BrowseButton_Click(object sender, RoutedEventArgs e) {
+			Microsoft.Win32.OpenFileDialog diag = new Microsoft.Win32.OpenFileDialog();
+			diag.InitialDirectory = Directory.GetCurrentDirectory();
+			diag.CheckFileExists = true;
+			diag.DefaultExt = "png";
+			diag.Filter = "PNG Image (*.png)|*.png";
+			diag.Multiselect = false;
+			
+
+			// Process open file dialog box results 
+			if (diag.ShowDialog() == true)
+			{ 
+				string filename = diag.FileName;
+				FilePath = filename;
+			}
+		}
+
+		private void TestTarget_Click(object sender, RoutedEventArgs e) {
+			//tempWindow.setWindowTitleRegex(TargetWindow);
+			//tempWindow.test();
+		}
 	}
 }
