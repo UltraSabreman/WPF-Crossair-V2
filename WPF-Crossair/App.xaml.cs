a6d4c688b3570af38952fb48faa38376596d9d6a
Andrey Byelogurov <byelogurov@gmail.com>
2015-02-27 12:18:32 -0800

    Embedded BindButton dll into project
    http://www.digitallycreated.net/Blog/61/combining-multiple-assemblies-into-a-single-exe-for-a-wpf-application

--- WPF-Crossair/App.xaml.cs
+++ WPF-Crossair/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Reflection;
 using System.IO;
 using System.Text;
+using System.Globalization;
 
 
 namespace WPF_Crosshair {
@@ -21,10 +22,7 @@ namespace WPF_Crosshair {
 		public App() {
 
 			AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(resolveJson);
-			//AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(resolveButton);
-#if !DEBUG
 			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(ErrorHandler);
-#endif
 
 		}
 
@@ -33,25 +31,24 @@ namespace WPF_Crosshair {
 			wr = new StreamWriter(filestream);
 			DumpMachineInfo();
 
-			PrintLine("++++Unhandled Exception Occured!++++");
+			PrintLine("==== Unhandled Exception Occured!");
 			Exception curEx = (Exception)args.ExceptionObject;
 
-			PrintLine("+ Type: " + args.ExceptionObject.GetType().ToString());
-			PrintLine("+ Throwie: " + curEx.TargetSite);
-			PrintLine("+ Message: " + curEx.Message);
+			PrintLine("= Type: " + args.ExceptionObject.GetType().ToString());
+			PrintLine("= Throwie: " + curEx.TargetSite);
+			PrintLine("= Message: " + curEx.Message);
 
 			StringBuilder Tabs = new StringBuilder();
 			while (curEx.InnerException != null) {
 				Tabs.Append("\t");
 				curEx = (Exception)curEx.InnerException;
-				PrintLine("+---Inner---+");
-				PrintLine(String.Format("+ {0}Type: " + curEx.GetType().ToString(), Tabs.ToString()));
-				PrintLine(String.Format("+ {0}Throwie: " + curEx.TargetSite, Tabs.ToString()));
-				PrintLine(String.Format("+ {0}Message: " + curEx.Message, Tabs.ToString()));
+				PrintLine("=--- Inner");
+				PrintLine(String.Format("= {0}Type: " + curEx.GetType().ToString(), Tabs.ToString()));
+				PrintLine(String.Format("= {0}Throwie: " + curEx.TargetSite, Tabs.ToString()));
+				PrintLine(String.Format("= {0}Message: " + curEx.Message, Tabs.ToString()));
 			}
-			PrintLine("+++++++++++++Stack Trace++++++++++++");
+			PrintLine("=== Stack Trace");
 			PrintLine("\n" + ((Exception)args.ExceptionObject).StackTrace);
-			PrintLine("++++++++++++++++++++++++++++++++++++");
 			wr.Close();
 			Environment.Exit(1);
 		}
@@ -62,35 +59,40 @@ namespace WPF_Crosshair {
 			foreach (Object o in args)
 				line.Append(o.ToString());
 
+			line.Append('\n');
 			wr.WriteLine(line.ToString());
 		}
 
 		void DumpMachineInfo() {
-			StringBuilder line = new StringBuilder();
-			line.Append(DateTime.Now.ToString("==== Date: MM-dd-yyyy hh:m:ss-fff tt===="));
-			line.Append("==== Name: " + Environment.MachineName + "====");
-			line.Append("==== Os: " + Environment.OSVersion + "====");
-			line.Append("==== Ver: " + Environment.Version + "====");
-			line.Append("==== Dir: " + Environment.CurrentDirectory + "====");
-
-			PrintLine(line.ToString());
+			PrintLine("==== Date: " + DateTime.Now.ToString("MM-dd-yyyy hh:m:ss-fff tt"));
+			//PrintLine("==== Name: " + Environment.MachineName);
+			PrintLine("==== Os: " + Environment.OSVersion);
+			PrintLine("==== Ver: " + Environment.Version);
+			PrintLine("==== Dir: " + Environment.CurrentDirectory);
 		}
 
-
+		//Embedding DLLs into WPF project via:
+		//http://www.digitallycreated.net/Blog/61/combining-multiple-assemblies-into-a-single-exe-for-a-wpf-application
 		static Assembly resolveJson(object sender, ResolveEventArgs args) {
-			 using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("WPF_Crosshair.Resources.Newtonsoft.Json.dll")) {
-				byte[] assemblyData = new byte[stream.Length];
-				stream.Read(assemblyData, 0, assemblyData.Length);
-				return Assembly.Load(assemblyData);
+			Assembly executingAssembly = Assembly.GetExecutingAssembly();
+			AssemblyName assemblyName = new AssemblyName(args.Name);
+ 
+			string path = assemblyName.Name + ".dll";
+			if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+			{
+				path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
 			}
-		}
+ 
+			using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+			{
+				if (stream == null)
+					return null;
 
-		static Assembly resolveButton(object sender, ResolveEventArgs args) {
-			using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BindButton.Bin.Debug.BindButton.dll")) {
-				byte[] assemblyData = new byte[stream.Length];
-				stream.Read(assemblyData, 0, assemblyData.Length);
-				return Assembly.Load(assemblyData);
+				byte[] assemblyRawBytes = new byte[stream.Length];
+				stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+				return Assembly.Load(assemblyRawBytes);
 			}
+
 		}
 		 
 
